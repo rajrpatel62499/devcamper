3bfa66afbd2656cfc239b40ff5811445d523c17f
Raj Patel <rajrpatel62499@gmail.com>
2021-04-26 14:07:26 +0530

    mongodb connection done with atlas

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
 
 const express = require('express');
 const dotenv = require('dotenv');
-// const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+
+// Load env vars
+dotenv.config({ path: `./config/config.env`});
+// connect to database 
+connectDB();
 
 // Route Files 
 const bootcamps = require('./routes/bootcamps');
 
-// Load env vars
-dotenv.config({ path: `./config/config.env`});
-
 const app = express();
 
-// app.use(logger);
 // Dev logging middleware 
 if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
 }
+
 // Mount Routers 
 app.use('/api/v1/bootcamps', bootcamps);
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` ))
+const server = app.listen(PORT, ()=> console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` ));
+
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // close server & exit process
+    server.close(() => process.exit(1));
+})
