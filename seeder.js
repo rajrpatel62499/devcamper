d92e26113ab12c3509736d0bea749deaba0d92d5
Raj Patel <rajangular.pingdr@gmail.com>
2021-04-30 14:22:56 +0530

    database seeder added

---
+++ seeder.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+
+// load env vars 
+dotenv.config({ path: './config/config.env'});
+
+// Load Models
+const Bootcamp = require('./models/Bootcamp');   
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/devcamper_project_resources/_data/bootcamps.json`));
+// const courses = JSON.parse(fs.readFileSync(`${__dirname}/devcamper_project_resources/_data/courses.json`));
+// const reviews = JSON.parse(fs.readFileSync(`${__dirname}/devcamper_project_resources/_data/reviews.json`));
+// const users = JSON.parse(fs.readFileSync(`${__dirname}/devcamper_project_resources/_data/users.json`));
+
+// Import into DB 
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+        console.log('Data Imported...'.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// Delete Data 
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+        console.log('Data Destroyed...'.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
+
+console.log(process.argv);
